9b81aaef16fc13e8f0f781d168cab2a5de37e22c
jay-isampelliwar <isampelliwar79@gmail.com>
2023-04-22 12:26:49 +0530

    added Routes

--- src/server.js
+++ src/server.js
@@ -3,12 +3,14 @@ require("dotenv").config();
 const bodyParser = require("body-parser");
 const app = express();
 const PORT = process.env.PORT || 3000;
-const userRoute = require();
+const userRoute = require("./routes/userRoutes");
+const userDataRoutes = require("./routes/userDataRoutes");
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 app.use("/user", userRoute);
+app.use("/user/data", userDataRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on ${PORT}`);
