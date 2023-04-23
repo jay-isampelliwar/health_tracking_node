44f5208ead78a0881ad3c1060474a5dc2fa72733
jay-isampelliwar <isampelliwar79@gmail.com>
2023-04-23 17:37:37 +0530

    added verify_otp

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -4,10 +4,12 @@ const {
   userDetails,
   userLogin,
   userRegistration,
+  verifyOTP,
 } = require("../controllers/userController");
 
 router.post("/registration", userRegistration);
 router.post("/login", userLogin);
 router.post("/", userDetails);
+router.post("/verify_otp", verifyOTP);
 
 module.exports = router;
