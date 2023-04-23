af4c36902ff6c7c577896ba896981b369d82d0cb
jay-isampelliwar <isampelliwar79@gmail.com>
2023-04-23 17:37:24 +0530

    added user_id attribute

--- src/models/userAchievementModel.js
+++ src/models/userAchievementModel.js
@@ -1,42 +1,44 @@
 const { Schema, model } = require("mongoose");
 
 const userAchievementModel = new Schema({
-  achievements: {
-    highest_point: {
-      type: Number,
-      default: 0,
-    },
-    highest_point_date: {
-      type: Date,
-    },
-    highest_distance: {
-      type: Number,
-      default: 0,
-    },
-    highest_distance_date: {
-      type: Date,
-    },
-    highest_water: {
-      type: Number,
-      default: 0,
-    },
-    highest_water_date: {
-      type: Date,
-    },
-    highest_step_count: {
-      type: Number,
-      default: 0,
-    },
-    highest_step_count_date: {
-      type: Date,
-    },
-    highest_calorie_burned: {
-      type: Number,
-      default: 0,
-    },
-    highest_calorie_burned_date: {
-      type: Date,
-    },
+  user_id: {
+    type: Schema.Types.ObjectId,
+    ref: "User",
+  },
+  highest_point: {
+    type: Number,
+    default: 0,
+  },
+  highest_point_date: {
+    type: Date,
+  },
+  highest_distance: {
+    type: Number,
+    default: 0,
+  },
+  highest_distance_date: {
+    type: Date,
+  },
+  highest_water: {
+    type: Number,
+    default: 0,
+  },
+  highest_water_date: {
+    type: Date,
+  },
+  highest_step_count: {
+    type: Number,
+    default: 0,
+  },
+  highest_step_count_date: {
+    type: Date,
+  },
+  highest_calorie_burned: {
+    type: Number,
+    default: 0,
+  },
+  highest_calorie_burned_date: {
+    type: Date,
   },
 });
 
